b7201075ecee29d3b4e3800d3318821eca1054a9
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-25 16:29:28 -0500

    added main pages and routing

--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachDetails from "./pages/coaches/CoachDetails.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+import NotFound from "./pages/NotFound.vue";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachesList },
     {
       path: "/coaches/:id",
-      component: null,
-      children: [{ path: "contact", component: null }], // /coaches/:id/contact
+      component: CoachDetails,
+      children: [{ path: "contact", component: ContactCoach }], // /coaches/:id/contact
     },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: CoachRegistration },
+    { path: "/requests", component: RequestsReceived },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
