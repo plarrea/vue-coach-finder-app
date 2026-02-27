bb9327008807036296f5d68be50c263eb797cfdb
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-27 16:55:07 -0500

    registration feature

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,24 @@
 <template>
-  <section>Coach Registration</section>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <CoachForm @save-data="saveData" />
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from "@/components/coaches/CoachForm.vue";
+
+export default {
+  components: {
+    CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch("coaches/registerCoach", data);
+      this.$router.replace("/coaches");
+    },
+  },
+};
+</script>
