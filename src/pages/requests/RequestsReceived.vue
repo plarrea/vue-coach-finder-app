0b11f2310ea659484a2d484a55044fc4fa8874d1
Pablo Larrea <larrea.pablo@gmail.com>
2026-03-04 12:36:14 -0500

    load request implementation

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,9 +1,13 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Received Requests</h2>
       </header>
+      <base-spinner v-if="isLoading" />
       <ul v-if="hasRequests">
         <RequestItem
           v-for="request in receivedRequests"
@@ -24,13 +28,37 @@ export default {
   components: {
     RequestItem,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters["requests/requests"];
     },
     hasRequests() {
-      return this.$store.getters["requests/hasRequests"];
+      return !this.isLoading && this.$store.getters["requests/hasRequests"];
     },
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      this.error = null;
+      try {
+        await this.$store.dispatch("requests/loadRequests");
+      } catch (error) {
+        this.error = error.message || "Something went wrong!";
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+  created() {
+    this.loadRequests();
   },
 };
 </script>
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,36 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
+      // coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const url = `${process.env.VUE_APP_FIRE_BASE_URL}/requests/${payload.coachId}.json`;
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(newRequest),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to send request.");
+    }
+    const responseData = await response.json();
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
     context.commit("addRequest", newRequest);
+  },
+  async loadRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const url = `${process.env.VUE_APP_FIRE_BASE_URL}/requests/${coachId}.json`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to load requests");
+    }
+    const responseData = await response.json();
+    const requests = [];
+    for (const key in responseData) {
+      const request = { id: key, coachId, ...responseData[key] };
+      requests.push(request);
+    }
+    context.commit("setRequests", requests);
   },
 };
