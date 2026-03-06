a1118b21b26387ed0a49bbc33bd4b1d976190418
Pablo Larrea <larrea.pablo@gmail.com>
2026-03-06 13:46:37 -0500

    logout implementation

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -49,4 +49,11 @@ export default {
       tokenExpiration: responseData.expiresIn,
     });
   },
+  logout(context) {
+    context.commit("setUser", {
+      userId: null,
+      token: null,
+      tokenExpiration: null,
+    });
+  },
 };
