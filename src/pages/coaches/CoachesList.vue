54488e3e8a581af7656243414ce0100bb4851ce6
Pablo Larrea <larrea.pablo@gmail.com>
2026-03-06 13:40:34 -0500

    added conditional login buttons

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -13,12 +13,17 @@
     <section>
       <base-card>
         <div class="controls">
-          <base-button mode="outline" @click="loadCoaches(true)"
-            >Refresh</base-button
+          <base-button mode="outline" @click="loadCoaches(true)">
+            Refresh
+          </base-button>
+          <base-button link to="/auth" v-if="!isLoggedIn"> Login </base-button>
+          <base-button
+            v-if="isLoggedIn && !isCoach && !isLoading"
+            link
+            to="/register"
           >
-          <base-button v-if="!isCoach && !isLoading" link to="/register"
-            >Register as a Coach</base-button
-          >
+            Register as a Coach
+          </base-button>
         </div>
         <base-spinner v-if="isLoading" />
         <ul v-else-if="hasCoaches">
@@ -59,6 +64,9 @@ export default {
     };
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
     filteredCoaches() {
       const coaches = this.$store.getters["coaches/coaches"];
       return coaches.filter((coach) => {
