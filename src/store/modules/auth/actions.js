f240e84d9c28731c41bcb55b2b596a3d1be1270e
Pablo Larrea <larrea.pablo@gmail.com>
2026-03-10 10:18:33 -0400

    auto login implementation

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,58 +2,59 @@ const ACCOUNTS_URL = "https://identitytoolkit.googleapis.com/v1/accounts";
 
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      `${ACCOUNTS_URL}:signInWithPassword?key=${process.env.VUE_APP_AUTH_API_KEY}`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      },
-    );
+    return context.dispatch("auth", { ...payload, mode: "login" });
+  },
+  async signup(context, payload) {
+    return context.dispatch("auth", { ...payload, mode: "signup" });
+  },
+  logout(context) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+
+    context.commit("setUser", {
+      userId: null,
+      token: null,
+      tokenExpiration: null,
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    const url = `${ACCOUNTS_URL}:${
+      mode === "signup" ? "signUp" : "signInWithPassword"
+    }?key=${process.env.VUE_APP_AUTH_API_KEY}`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     if (!response.ok) {
       throw new Error("Failed to login.");
     }
 
     const responseData = await response.json();
+    localStorage.setItem("token", responseData.idToken);
+    localStorage.setItem("userId", responseData.localId);
+
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
     });
   },
-  async signup(context, payload) {
-    const response = await fetch(
-      `${ACCOUNTS_URL}:signUp?key=${process.env.VUE_APP_AUTH_API_KEY}`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      },
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to signup.");
+  tryLogin(context) {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    if (token && userId) {
+      context.commit("setUser", {
+        token,
+        userId,
+        tokenExpiration: null,
+      });
     }
-
-    const responseData = await response.json();
-    context.commit("setUser", {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
-  },
-  logout(context) {
-    context.commit("setUser", {
-      userId: null,
-      token: null,
-      tokenExpiration: null,
-    });
   },
 };
